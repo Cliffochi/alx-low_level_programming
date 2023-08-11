e7f776de05a369ecf67e2087f1bb89fdcfee2c41
Cliffochi <cliffhani4@gmail.com>
2023-08-11 05:12:00 -0700

    hexa alphabet

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print all numbers of base64 in lowercase.
+ * You can only use `putchar`, and only 3 times.
+ * Return: 0
+ */
+int main(void)
+{
+	char q;
+	int w;
+
+	for (w = 0;w < 10; w++)
+	{
+		putchar(w + '0');
+	}
+	for (q = 'a'; q < 'g'; q++)
+	{
+		putchar(q);
+	}
+	putchar('\n');
+
+	return (0);
+}
