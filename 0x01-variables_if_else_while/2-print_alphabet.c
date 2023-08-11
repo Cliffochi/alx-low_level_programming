9f1079b45c02c7356a653702faa153941c5639ca
Cliffochi <cliffhani4@gmail.com>
2023-08-11 03:47:31 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * msin - main code block
+ * using putchar to display alphatet
+ * Return: 0 ==> success
+*/
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
