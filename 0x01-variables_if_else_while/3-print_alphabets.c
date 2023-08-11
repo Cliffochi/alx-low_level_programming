94ee6543b39b384e5cbc504767797266d25cc1e7
Cliffochi <cliffhani4@gmail.com>
2023-08-11 04:05:24 -0700

    semicased alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Uses `putchar` to print lowercase and then uppercase alphabet.
+ * Return: 0
+ */
+int main(void)
+{
+	char q = 'a';
+	
+	while (q <= 'z')
+	{
+		putchar(q);
+		q++;
+	}
+
+	q = 'A';
+
+	while (q <= 'Z')
+	{
+		putchar(q);
+		q++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
