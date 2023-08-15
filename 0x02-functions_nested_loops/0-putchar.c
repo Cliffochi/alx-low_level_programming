4e047f995488dbba95e533b6737fe602ba82188d
Cliffochi <cliffhani4@gmail.com>
2023-08-15 04:23:24 -0700

    _putcharrrr

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,24 @@
 #include<unistd.h>
+#include<main.h>
+
 /**
- * main - Entry point
+ * _putchar - writes the character c to stdout
+ *@c: The character to print
  *
- * Return: Alwasys 0
- *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 
 int main(void)
 {
