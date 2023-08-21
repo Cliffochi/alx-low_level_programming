c6e36770cb146a759328fcf4733b427394f02079
Cliffochi <cliffhani4@gmail.com>
2023-08-21 04:26:45 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints string to stdout
+ * @str: char array sting type
+ */
+
+void _puts(char *str)
+{
+	int w;
+	
+	for (w = 0; str[i] != '\0', w++)
+	{
+		_putchar(str[w]);
+	}
+	_putchar('\n');
+}
