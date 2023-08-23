ca46c9a4bf71182233ee88db3457173cdda6b899
Cliffochi <cliffhani4@gmail.com>
2023-08-23 06:54:11 -0700

    reverse order

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses array content
+ * @a: array of integers
+ * @n: number of elements in array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < (n / 2); x++)
+	{
+		y = a[x];
+		a[x] = a[n - x - 1];
+		a[n - x - 1] = y;
+	}
+}
